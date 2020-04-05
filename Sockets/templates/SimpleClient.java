374d0f101713b4f4ee129913398f877255575614
Sophie Engle <sjengle@cs.usfca.edu>
2020-04-05 16:44:51 -0700

    Updated socket code.

---
+++ Sockets/templates/SimpleClient.java
@@ -0,0 +1,27 @@
+import java.io.Closeable;
+
+public class SimpleClient {
+
+  public static void main(String[] args) throws Exception {
+    try (
+        Closeable placeholder = () -> { /* TODO: DELETE ME */ };
+    ) {
+
+      System.out.println("Client: Started...");
+      String input = "";
+
+      while (true) { // TODO
+        // TODO
+
+        if (input.equals(SimpleServer.EOT)) {
+          System.out.println("Client: Ending client.");
+          // TODO
+        }
+        else if (input.equals(SimpleServer.EXIT)) {
+          System.out.println("Client: Shutting down server.");
+          // TODO
+        }
+      }
+    }
+  }
+}
---
+++ Sockets/templates/SimpleServer.java
@@ -0,0 +1,39 @@
+import java.io.Closeable;
+
+public class SimpleServer {
+  public static final int PORT = 5554;
+  public static final String EOT = "EOT";
+  public static final String EXIT = "SHUTDOWN";
+
+  public static void main(String[] args) throws Exception {
+    String line = "";
+
+    try (
+        Closeable placeholder1 = () -> { /* TODO: DELETE ME */ };
+    ) {
+      while (true) { // TODO
+        System.out.println("Server: Waiting for connection...");
+
+        try (
+            Closeable placeholder2 = () -> { /* TODO: DELETE ME */ };
+        ) {
+          while (true) { // TODO
+            // TODO
+            System.out.println("Server: " + line);
+
+            if (line.equals(EOT)) {
+              System.out.println("Server: Closing socket.");
+              // TODO
+            } else if (line.equals(EXIT)) {
+              System.out.println("Server: Shutting down.");
+              // TODO
+            }
+          }
+        }
+
+        // TODO
+        // System.out.println("Server: Client disconnected.");
+      }
+    }
+  }
+}
